208a4e599c571912a0aa52078acab59e6056302f
Miriam Posner <miriam.posner@gmail.com>
2015-09-13 18:58:24 -0700

    corrected some syntax

--- selenium_scrape.py
+++ selenium_scrape.py
@@ -6,8 +6,8 @@ type(browser)
 
 browser.get('http://digital2.library.ucla.edu/Search.do?keyWord=&selectedProjects=27&pager.offset=50&viewType=1&maxPageItems=1000')
 
-pageItems = browser.find_elements_by_class_name('searchTitle')
-for link in pageItems:
-    element.click(link)
+links = browser.find_elements_by_class_name('searchTitle')
+for link in links:
+    link.click()
     browser.back()
     print("clicked!")
